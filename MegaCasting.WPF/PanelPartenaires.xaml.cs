d59bf35f686680265f42b22e4fe69cbf7dd9a55d
jeoff09 <jeoffrey.godart@hotmail.fr>
2015-05-01 14:39:03 +0200

    mise en place de sécurité

--- MegaCasting.WPF/PanelPartenaires.xaml.cs
+++ MegaCasting.WPF/PanelPartenaires.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MegaCasting.DBLib;
+using MegaCasting.WPF.GestionWindows;
+using MegaCasting.WPF.ViewModel;
 
 namespace MegaCasting.WPF
 {
@@ -23,6 +26,29 @@ namespace MegaCasting.WPF
         public PanelPartenaires()
         {
             InitializeComponent();
+        }
+
+        private void ButtonPartenaireAjouter_Click(object sender, RoutedEventArgs e)
+        {
+            AjoutPartenaire nouveauPartenaire = new AjoutPartenaire();
+            nouveauPartenaire.ShowDialog();
+        }
+
+        private void ButtonPartenaireSupprimer_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Voulez-vous vraiment supprimer ce partenaire ?", "Suppression d'un partenaire", MessageBoxButton.YesNoCancel);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    
+                    break;
+                case MessageBoxResult.No:
+                    
+                    break;
+                case MessageBoxResult.Cancel:
+                   
+                    break;
+            }
         }
     }
 }
