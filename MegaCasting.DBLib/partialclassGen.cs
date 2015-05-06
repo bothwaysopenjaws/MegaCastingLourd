b157c6ef8746bc1b9756022bfaccafb9711e28aa
Manu/Morpheus <e.stephant@iia-laval.fr>
2015-05-06 09:14:07 +0200

    washed brain
    lkjlkjlkjlkjlkjlkj

---
+++ MegaCasting.DBLib/partialclassGen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaCasting.DBLib
+{
+    public partial class Offre
+    {
+
+
+        //public bool isChecked
+        //{
+        //    get { return offre; }
+        //    offre
+        //}
+        
+    }
+}
